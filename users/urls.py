aef855a8a1008ab73d5e2061d5699cf18876abf7
Shah Rukh Khan <shah@crowdbotics.com>
2020-09-02 06:46:40 +0000

    Update models

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import (
     user_redirect_view,
     user_update_view,
-    UserDetailView,
     UserUpdateView,
     user_detail_view,
+    UserDetailView,
 )
 
 app_name = "users"
